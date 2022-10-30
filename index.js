72481fe5dc2021817a141fbe0f2682c963e5f668
PanicAtTheCisco <83227932+PanicAtTheCisco@users.noreply.github.com>
2022-10-30 15:49:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,9 @@
 /**
  * IMPORTANT: 
- * > THIS BOT IS A PROOF OF CONECPT, ONLY USE ON SERVERS YOU HAVE PERMISSION TO : DO NOT USE FOR MALICIOUS PURPOSES
- * > DEVELOPER IS NOT RESPONSIBLE FOR ANY DAMAGE DONE WITH BOT BY OTHERS
+ * > THIS BOT IS A PROJECT TO LEARN MORE ABOUT JS AND DISCORD BOTS, ONLY USE ON SERVERS YOU HAVE PERMISSION TO : DO NOT USE FOR MALICIOUS PURPOSES
+ * > DEVELOPER IS NOT RESPONSIBLE FOR ANYTHING DONE WITH BOT BY OTHERS
  * 
- * REQUIREMENTS:
- * > node.js installed
- * > discord.js npm packaged installed
- * 
- * HOW TO START:
- * > Setup bot as an application on your discord developer portal (https://discord.com/developers/applications)
- * > Create invite link using Discord's built in link generator and make sure the bot's permissions are set to only administrator 
- * > Add bot to server
- * > Run 'node .' in code to start bot
- * 
- * > This bot is meant to be used as a privilege escalation tool for Discord servers
- * > Bot can be customized to be used however is needed to maintain cover, though the default is the 'Funny Bot' cover 
- * and customizing requires some discord.js knowledge
- * > Commands can be added and removed for modularity, again, some discord.js knowledge is needed
- * 
- * 
- * NOTE:
- * > Bot must be on the server for use
- * > There are still a few bugs being worked with the '-create' and '-add' commands so expect a few issues
- * > There is no multi-command handling so if more than one person tries to use a command at the same time nothing will happen for either of them.
- * If needed on multiple servers it is recommened to host different instances of the bot
+ * LOOK AT READ ME FOR MORE INFORMATION
  */
 
 const Discord = require('discord.js');
